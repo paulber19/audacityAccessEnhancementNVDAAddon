12c9016e58e7883cb0185b6ca9490925537299ed
paulber19 <paulber19@laposte.net>
2022-03-19 06:30:14 +0100

    version 2.6.2

--- buildVars.py
+++ buildVars.py
@@ -46,7 +46,7 @@ Previous versions of Audacity are not  supported.
 """),
 
 	# version
-	"addon_version": "2.6.1",
+	"addon_version": "2.6.2",
 	# Author(s)
 	"addon_author": "paulber19",
 	# URL for the add-on documentation support
